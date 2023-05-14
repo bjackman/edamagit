0a61fd4585e6066f8fc3001960aebf562e1093d1
Brendan Jackman <bhenryj0117@gmail.com>
2023-05-14 21:26:57 +0200

    truncate unsourced commit lists
    The previous commit made it relatively fast to gather this infor for a
    large number of commits, but the status view is still very large in a
    way that isn't very useful, let's truncate it like the original Magit
    does
    (https://github.com/kahole/edamagit/issues/255#issuecomment-1544651493)

--- src/views/commits/unsourcedCommitsSectionView.ts
+++ src/views/commits/unsourcedCommitsSectionView.ts
@@ -8,12 +8,19 @@ import { MagitCommitSummary } from '../../models/magitCommit';
 export class UnsourcedCommitSectionView extends View {
   isFoldable = true;
 
+  static maxEntries = 256;
+
   get id() { return this.section.toString(); }
 
   constructor(private section: Section, upstream: UpstreamRef, commits: MagitCommitSummary[], refs: Ref[]) {
     super();
+    let truncated = false;
+    if (commits.length > UnsourcedCommitSectionView.maxEntries) {
+      commits = commits.slice(0, UnsourcedCommitSectionView.maxEntries);
+      truncated = true;
+    }
     this.subViews = [
-      new SectionHeaderView(section, commits.length, `${upstream.remote}/${upstream.name}`),
+      new SectionHeaderView(section, commits.length, `${upstream.remote}/${upstream.name}`, truncated),
       ...commits.map(commit => new CommitItemView(commit, undefined, refs)),
       new LineBreakView()
     ];
